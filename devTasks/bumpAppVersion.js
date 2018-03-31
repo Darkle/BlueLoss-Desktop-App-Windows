a659c86bccf3c7d9de38fc4cf75eb38f2eb2d8bc
Coop Coding <coop.coding@gmail.com>
2018-03-31 17:03:03 +1100

    Updated bumpAppVersion Gulp task.

--- devTasks/bumpAppVersion.js
+++ devTasks/bumpAppVersion.js
@@ -9,12 +9,15 @@ const moment = require('moment')
 const updateInfoJsonFilePath = path.join(__dirname, '..', 'updateInfo.json')
 const settingsDefaultsFilePath = path.join(__dirname, '..', 'app', 'settings', 'settingsDefaults.lsc')
 
-const newVersionString = `${moment().year()}.${moment().month() + 1}.${moment().date()}`
+const newVersionString = `${ moment().year() }.${ moment().month() + 1 }.${ moment().date() }`
 
+/**
+ * We bump the package.json with npm (which also adds a git tag with the version),
+ * also updateInfo.json and in settingsDefaults.lsc.
+ */
 gulp.task('bumpVersion', () => {
-  console.log(`running 'npm version ${newVersionString}' and updating version in updateInfo.json and in settingsDefaults.lsc`)
-  return exeq(`npm version ${newVersionString}`)
-    // Update the updateInfo.json too
+  console.log(`running 'npm version ${ newVersionString }' and updating version in updateInfo.json and in settingsDefaults.lsc`)
+  return exeq(`npm version ${ newVersionString }`)
     .then(() => jetpack.writeAsync(updateInfoJsonFilePath, { latestVersion: newVersionString }))
     .then(() => jetpack.readAsync(settingsDefaultsFilePath))
     .then(result =>
