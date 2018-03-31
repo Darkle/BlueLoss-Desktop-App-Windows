054c0434ba7162231a2c32dc041dedba47455468
Coop Coding <coop.coding@gmail.com>
2018-03-31 15:07:28 +1100

    Added a bump version gulp task.

--- app/settingsWindow/renderer/settingsWindowRendererMain-compiled.js
+++ app/settingsWindow/renderer/settingsWindowRendererMain-compiled.js
@@ -104,7 +104,7 @@ const defaultSettings = {
   dateLastCheckedForOUIupdate: Date.now(),
   settingsWindowPosition: null,
   dateLastCheckedForAppUpdate: Date.now(),
-  skipUpdateVersion: '2018.3.30'
+  skipUpdateVersion: '2018.3.39'
 };
 
 exports.defaultSettings = defaultSettings;
--- devTasks/bumpAppVersion.js
+++ devTasks/bumpAppVersion.js
@@ -8,25 +8,25 @@ const moment = require('moment')
 const updateInfoJsonFilePath = path.join(__dirname, '..', 'updateInfo.json')
 const settingsDefaultsFilePath = path.join(__dirname, '..', 'app', 'settings', 'settingsDefaults.lsc')
 
-const newVersionString = `${moment().year()}.${moment().month() + 1}.${moment().date()+1}`
+const newVersionString = `${moment().year()}.${moment().month() + 1}.${moment().date()}`
 
 gulp.task('bumpVersion', () => {
   console.log(`running 'npm version ${newVersionString}' and updating version in updateInfo.json and in settingsDefaults.lsc`)
   return exeq(`npm version ${newVersionString}`)
     // Update the updateInfo.json too
-    .then(jetpack.writeAsync(updateInfoJsonFilePath, { latestVersion: newVersionString }))
-    .then(jetpack.readAsync(settingsDefaultsFilePath))
-    .then(result => {
-      return result.split(/\r\n?|\n/).map(line => {
+    .then(() => jetpack.writeAsync(updateInfoJsonFilePath, { latestVersion: newVersionString }))
+    .then(() => jetpack.readAsync(settingsDefaultsFilePath))
+    .then(result =>
+      result.split(/\r\n?|\n/).map(line => {
         if(!line.includes('skipUpdateVersion') || line.includes('string')) {
           return line
         }
-        return `skipUpdateVersion: '${newVersionString}',`
+        return `skipUpdateVersion: '${ newVersionString }',`
       }).join('\n')
-    })
-    .then(newFileData => {
+    )
+    .then(newFileData =>
       jetpack.writeAsync(settingsDefaultsFilePath, newFileData)
-    })
+    )
     .then(() => {
       console.log('Successfully bumped LANLost version!')
     })
